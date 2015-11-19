0922b386934fb4bfb569ec45e6aec92c90a4213a
injune1123 <injune1123@Juns-MacBook-Air.local>
2015-11-19 16:28:58 -0500

    made home  page into a huge jumborton and changed the greeting

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -5,16 +5,15 @@ app.factory('RandomGreetings', function () {
     };
 
     var greetings = [
-        'Hello, world!',
-        'At long last, I live!',
+        'Sport up!',
+        'teamwork makes the dream work',
         'Hello, simple human.',
-        'What a beautiful day!',
-        'I\'m like any other project, except that I am yours. :)',
-        'This empty string is for Lindsay Levine.',
-        'こんにちは、ユーザー様。',
-        'Welcome. To. WEBSITE.',
-        ':D',
-        'Yes, I think we\'ve met before.'
+        'What a beautiful day!Let\'s work out!',
+        '生命在于运动',
+        'Life is short, Play Hard!',
+        'Together Everyone Achieves More.',
+        'United we play. United we win.',
+        'You may be strong but we are stronger.'
     ];
 
     return {
