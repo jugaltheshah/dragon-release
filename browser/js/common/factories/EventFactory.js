c3051f8ced2f860d13a9990a56f48c4da241db84
Bryce Frost <bfrost@bfrost-macbookpro3.roam.corp.google.com>
2015-12-05 17:00:17 -0500

    updated the event list page and search filters

--- browser/js/common/factories/EventFactory.js
+++ browser/js/common/factories/EventFactory.js
@@ -25,7 +25,6 @@ app.factory('EventFactory', function($http){
 
     function getEvents() {
         return $http.get('/api/events').then(function(res){
-            console.log(res);
             return res.data;
         });
     }
