e73764e33cd2996f882108d14c09f4ea29c08550
MSun <msun@MM63768M1.local>
2015-12-12 16:22:07 -0500

    Check google location api result

--- browser/js/event/event.js
+++ browser/js/event/event.js
@@ -57,7 +57,7 @@ app.config(function($stateProvider){
 
                 $scope.events = $scope.events.filter(function(event){
                   var d = new Date(event.date).getTime()
-                
+
                   if (d == t){
                     console.log('Match!');
                     return event
@@ -100,7 +100,7 @@ app.config(function($stateProvider){
                     case 'nextweek':
                       then = new Date().setDate(now.getDate()+14);
 
-                  }                  
+                  }
                   console.log(then);
 
                   var filteredEvents = $scope.events.filter(function(event){
@@ -203,6 +203,7 @@ app.config(function($stateProvider){
                     var addressString_post = addressString_pre.split(' ').join('+');
                     Utils.getCoordinates(addressString_post)
                         .then(function(res){
+                            console.log(res);
                             $scope.location = (res.data.status === 'OK') ? res.data.results[0].geometry.location : null;
                             return EventFactory.createEvent($scope.event)
                         })
